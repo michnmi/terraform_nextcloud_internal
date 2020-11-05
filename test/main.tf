3be04153b8aefc56ce872597c9c7b075d1527780
Michalis Michaloliakos <michalis@hashicorp.com>
2020-11-05 11:44:16 +0000

    VIR-38: Move all test VMs to vmhost02

--- test/main.tf
+++ test/main.tf
@@ -14,12 +14,18 @@ provider "libvirt" {
   // uri   = "qemu+ssh://vmhost01/system"
 }
 
+provider "libvirt" {
+  alias = "vmhost02"
+  uri   = "qemu+ssh://jenkins_automation@vmhost02/system?keyfile=../id_ed25519_jenkins"
+  // uri   = "qemu+ssh://vmhost02/system"
+}
+
 variable "env" {
   type = string
 }
 
 resource "libvirt_volume" "nextcloud" {
-  provider         = libvirt.vmhost01
+  provider         = libvirt.vmhost02
   name             = "nextcloud_${var.env}.qcow2"
   pool             = var.env
   base_volume_name = "nextcloud_base.qcow2"
@@ -28,15 +34,15 @@ resource "libvirt_volume" "nextcloud" {
 }
 
 resource "libvirt_domain" "nextcloud" {
-  provider  = libvirt.vmhost01
+  provider  = libvirt.vmhost02
   name      = "nextcloud_${var.env}"
   memory    = "512"
-  vcpu      = 1
+  vcpu      = 2
   autostart = true
 
   // The MAC here is given an IP through mikrotik
   network_interface {
-    macvtap  = "enp0s25"
+    macvtap  = "enp3s0"
     mac      = "52:54:00:EA:17:58"
     hostname = "nextcloud_${var.env}"
   }
